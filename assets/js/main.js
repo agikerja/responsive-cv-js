6abd6affd1f900ea6fbf7c075dacb2e59284c67d
agikerja <agibuatkerja@gmail.com>
2023-03-02 15:39:47 +0800

    progress home page

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+/*Show menu on hidden*/
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+
+/*Menu Show */
+/*Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*Menu Hidden */
+/*Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*Remove Menu Mobile */
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
